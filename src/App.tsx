7baa680c05a9db4cf091174ae5c784b1425e9d0d
Finn <nghia.trantrong0905@gmail.com>
2026-06-19 11:33:39 +0700

    init: first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-sans text-3xl'>Dự án final app tại Itern NUS!</h1>
+    </>
+  )
+}
+
+export default App
