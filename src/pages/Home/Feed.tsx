67356f925fbdcc15253d044cd41157aa377804a2
Finn <nghia.trantrong0905@gmail.com>
2026-06-21 16:06:12 +0700

    refactor: seperate photo modal into a components

--- src/pages/Home/Feed.tsx
+++ src/pages/Home/Feed.tsx
@@ -3,6 +3,7 @@ import { useInView } from 'react-intersection-observer';
 import PhotoCard from '../../components/PhotoCard';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import LoadingSpinner from '../../components/LoadingSpinner';
+import PhotoModal from '../../components/PhotoModal';
 
 
 const MOCK_DATA = [
@@ -336,31 +337,16 @@ const Feed = () => {
   // State for modal viewing photo/album
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
-  const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleModalOpen = (data) => {
     console.log(data);
     setSelectedItem(data);
     setIsModalOpen(true);
-    setCurrentIndex(0);
   };
 
   const handleModalClose = () => {
     setSelectedItem(null);
     setIsModalOpen(false);
-    setCurrentIndex(0);
-  };
-
-  const prevSlide = () => {   
-    setCurrentIndex((index) => {
-      return index === 0 ? selectedItem.media.image_stack.length - 1 : index - 1;
-    });
-  };
-
-  const nextSlide = () => {
-    setCurrentIndex((index) => {
-      return index === selectedItem.media.image_stack.length - 1 ? 0 : index + 1;
-    });
   };
 
   if (status === 'error') return <div>Error fetching data</div>;
@@ -405,7 +391,7 @@ const Feed = () => {
           {/* Attach the ref to this invisible div at the bottom */}
           <div ref={bottomRef} style={{ height: '50px' }}></div>
           {isFetchingNextPage && <LoadingSpinner />}
-          {!hasNextPage && <div>You've caught up on everything! 🎉</div>}
+          {!hasNextPage && <div>Bạn đã tìm kiếm hết mọi thứ có thể. 🎉</div>}
           </>}
         </div>
 
@@ -415,6 +401,7 @@ const Feed = () => {
         </aside>
       </div>
 
+      {/* Tab Bar dưới đáy cho giao diện web */}
       <div className="fixed bottom-0 right-0 left-0 sm:hidden bg-white border-t border-gray flex justify-around h-16 z-50">
         <button className="flex-1 flex flex-col items-center justify-center gap-1 cursor-pointer">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className="flex-1 h-8 w-8"><path fill="#3c5a9a" d="M213.1 128.8L202.7 160L128 160C92.7 160 64 188.7 64 224L64 480C64 515.3 92.7 544 128 544L512 544C547.3 544 576 515.3 576 480L576 224C576 188.7 547.3 160 512 160L437.3 160L426.9 128.8C420.4 109.2 402.1 96 381.4 96L258.6 96C237.9 96 219.6 109.2 213.1 128.8zM320 256C373 256 416 299 416 352C416 405 373 448 320 448C267 448 224 405 224 352C224 299 267 256 320 256z"/></svg>
@@ -427,29 +414,7 @@ const Feed = () => {
       </div>
 
       {/* Render modal hiển thị chi tiết ảnh */}
-      {isModalOpen && selectedItem && <div className="fixed top-0 bottom-0 right-0 left-0 bg-black/50 flex justify-center items-center z-1000">
-        <div className="rounded-sm w-[400px] h-[300px] sm:w-[500px] md:w-[800px] sm:h-[400px] md:h-[600px] bg-white p-2.5 flex flex-col items-center gap-2.5">
-          {/* Modal chứa title, các ảnh và description ảnh*/}
-          <div className="flex flex-row items-center justify-between w-full">
-            <span className="font-semibold text-xl line-clamp-1">{selectedItem.content.title}</span>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className="w-5 h-5 cursor-pointer" onClick={() => handleModalClose()}><path fill="#848585" d="M504.6 148.5C515.9 134.9 514.1 114.7 500.5 103.4C486.9 92.1 466.7 93.9 455.4 107.5L320 270L184.6 107.5C173.3 93.9 153.1 92.1 139.5 103.4C125.9 114.7 124.1 134.9 135.4 148.5L278.3 320L135.4 491.5C124.1 505.1 125.9 525.3 139.5 536.6C153.1 547.9 173.3 546.1 184.6 532.5L320 370L455.4 532.5C466.7 546.1 486.9 547.9 500.5 536.6C514.1 525.3 515.9 505.1 504.6 491.5L361.7 320L504.6 148.5z"/></svg>
-          </div>
-          <div className="relative flex-1 w-full h-full overflow-hidden flex items-center justify-center">
-            {selectedItem.media.type === "album" && (
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className="absolute top-1/2 left-4 -translate-y-1/2 w-20 h-40 hover:text-gray-200 transition-colors z-10 cursor-pointer select-none" onClick={prevSlide}>
-              <path fill="#848585" d="M169.4 297.4C156.9 309.9 156.9 330.2 169.4 342.7L361.4 534.7C373.9 547.2 394.2 547.2 406.7 534.7C419.2 522.2 419.2 501.9 406.7 489.4L237.3 320L406.6 150.6C419.1 138.1 419.1 117.8 406.6 105.3C394.1 92.8 373.8 92.8 361.3 105.3L169.3 297.3z"/>
-            </svg>)}
-
-            {<img src={selectedItem.media.image_stack[currentIndex].url} alt={selectedItem.media.image_stack[currentIndex].alt_text} className="w-full h-full object-contain"/>}
-
-            {selectedItem.media.type === "album" && (
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className="absolute top-1/2 right-4 -translate-y-1/2 w-20 h-40 hover:text-gray-200 transition-colors z-10 cursor-pointer select-none" onClick={nextSlide}>
-              <path fill="#848585" d="M471.1 297.4C483.6 309.9 483.6 330.2 471.1 342.7L279.1 534.7C266.6 547.2 246.3 547.2 233.8 534.7C221.3 522.2 221.3 501.9 233.8 489.4L403.2 320L233.9 150.6C221.4 138.1 221.4 117.8 233.9 105.3C246.4 92.8 266.7 92.8 279.2 105.3L471.2 297.3z"/>
-            </svg>)}
-          </div>
-          <p className="text-xs line-clamp-2">{selectedItem.content.body}</p>
-        </div>
-      </div>}
+      {isModalOpen && selectedItem && <PhotoModal data={selectedItem} handleModalClose={handleModalClose} />}
       {/* Thêm padding để main content không bị che lấp bởi tab bar */}
       <div className="pb-16 sm:pb-0">
         {/* Content của tab bar hiển thị ở đây. */}
