f6b53fd5d4c6db6aaa11d0fcddd979334d659e3f
Finn <nghia.trantrong0905@gmail.com>
2026-07-02 16:00:51 +0700

    feat: Refactor email service to use centralized mailer configuration

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -1,21 +1,5 @@
-import nodemailer from 'nodemailer';
 import { constant } from '../config/constant/constant.js';
-
-const transporter = nodemailer.createTransport({
-  host: constant.SMTP_HOST,
-  port: Number(constant.SMTP_PORT),
-  secure: false, // BẮT BUỘC: false cho port 587
-  auth: {
-    user: constant.SMTP_USER,
-    pass: constant.SMTP_PASS,
-  },
-  ...(constant.NODE_ENV === 'development'
-    ? {
-        logger: true,
-        debug: true,
-      }
-    : {}),
-} as nodemailer.TransportOptions);
+import transporter from '../config/mail/mailer.config.js';
 
 export const sendWelcomeAndVerifyEmail = async (
   to: string,
